603eb3698b897716f75a50f0fb005babef2aebea
Bruce <813703110@qq.com>
2022-04-09 12:29:48 +0800

    till ex33

---
+++ ex33.py
@@ -0,0 +1,41 @@
+# i = 0
+numbers = []
+
+# while i < 6:
+    # print(f"At the top i is {i}")
+    # numbers.append(i)
+    
+    # i=i+2
+    # print("Numbers now:", numbers)
+    # print(f"At the bottom i is {i}")
+for i in range(0, 6):
+    print(f"At the top i is {i}")
+    numbers.append(i)
+    
+    i=i+1
+    print("Numbers now:", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers:")
+
+for num in numbers:
+    print(num)
+
+# def abc(stop_num):
+    # i = 0
+    # numbers = []
+
+    # while i < stop_num:
+        # print(f"At the top i is {i}")
+        # numbers.append(i)
+        
+        # i = i + 1
+        # print("Numbers now: ", numbers)
+        # print(f"At the bottom i is {i}")
+            
+    # print("The numbers: ")
+
+    # for num in numbers:
+        # print(num)
+
+# abc(10)   
