3df9c94abfd2bdafe04b99b94d8ffb4b943cbeb6
bruce <813703110@qq.com>
2022-04-11 14:43:07 +0800

    ex43 not complished

---
+++ ex40_1.py
@@ -0,0 +1,24 @@
+happy_bday = ["Happy birthday to you",
+               "I don't want to get sued",
+               "So I'll stop right there"]
+                   
+bulls_on_parade = ["They rally around tha family",
+                    "With pockets full of shells"]
+                        
+# print("\n".join(happy_bday))
+
+# print("\n".join(bulls_on_parade))
+
+def one_by_one(one):
+    for i in one:
+        print(i)
+
+# print(happy_bday)
+
+
+one_by_one(happy_bday)
+
+one_by_one(bulls_on_parade)
+
+# for x in happy_bday:
+        # print(x)
--- ex40b.py
+++ ex40b.py
@@ -1,2 +1,6 @@
 import mystuff
 mystuff.apple()
+
+mystuff['apple'] # get apple from dict
+mystuff.apple() # get apple from the module
+mystuff.tangerine # same thing, it's just a variable
